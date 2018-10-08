b6cee7bb1cefcbc32336f0da6b877dc24a20a0aa
wcalvert88 <wcalvert88@gmail.com>
2018-10-08 11:01:51 -0400

    restricted movement to the game play area only

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 	// Use this for initialization
 	void Start () {
-		
+        SetUpMoveBoundaries();		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update () {
         Move();
 	}
 
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        Debug.Log(deltaX);
-        var newXPos = transform.position.x + deltaX;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        Debug.Log(deltaY);
-        var newYPos = transform.position.y + deltaY;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
