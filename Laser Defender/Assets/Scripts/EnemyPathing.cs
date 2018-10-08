ecf330c8fd972dc3665287a4962462829a0bbac7
wcalvert88 <wcalvert88@gmail.com>
2018-10-08 16:00:07 -0400

    set enemy move path

--- Laser Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -5,4 +5,40 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour {
 
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                targetPosition,
+                movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
