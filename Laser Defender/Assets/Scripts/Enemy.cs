c3a1ecd32c3562b2146be40c969dcf4d63c14b08
wcalvert88 <wcalvert88@gmail.com>
2018-10-09 14:43:39 -0400

    made enemies able to shoot back

--- Laser Defender/Assets/Scripts/Enemy.cs
+++ Laser Defender/Assets/Scripts/Enemy.cs
@@ -5,19 +5,48 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float health = 100;
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField] float shotCounter;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] float projectileSpeed = 10f;
+    // Use this for initialization
+    void Start () {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        CountDownAndShoot();
 	}
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+                    laserPrefab,
+                    transform.position,
+                    Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
         health -= damageDealer.GetDamage();
         if (health <= 0)
         {
